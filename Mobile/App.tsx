565b4b81b9c2341bd1c6b3c4e3681d2e030d7ed2
Brian <brianz1406b@gmail.com>
2026-09-17 15:32:22 -0400

    codigo correjido de problema de libreria

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -3,7 +3,7 @@
 
 
 import React from "react";
-import AppNavigator from "src/View/Navigation/AppNavigator";
+import AppNavigator from "./src/View/Navigation/AppNavigator";
 
 export default function App() {
   return <AppNavigator />;
